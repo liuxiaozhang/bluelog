739fa96bc69099f52976724633b10ba8d31abd27
root <205028388@qq.com>
2020-09-26 22:58:28 +0800

    factory

--- bluelog/__init__.py
+++ bluelog/__init__.py
@@ -2,39 +2,62 @@ import os
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
-from flask_debugtoolbar import DebugToolbarExtension
 
 from bluelog.blueprints.admin import admin_bp
 from bluelog.blueprints.auth import auth_bp
 from bluelog.blueprints.blog import blog_bp
+from bluelog.extensions import bootstrap, db, toolbar
 from bluelog.settings import configs
 
-app = Flask('bluelog')
-config_name = os.getenv('FLASK_CONFIG', 'development')
-app.config.from_object(configs[config_name])
+def create_app(config_name=None):
+    if config_name is None:
+        config_name = os.getenv('FLASK_CONFIG', 'development')
+    app = Flask('bluelog')
+    app.config.from_object(configs[config_name])
 
-bootstrap = Bootstrap(app)
-db = SQLAlchemy(app)
-toolbar = DebugToolbarExtension(app)
+    register_logging(app)
+    register_extensions(app)
+    register_blueprints(app)
+    register_shell_context(app)
+    register_template_context(app)
+    register_errors(app)
+    register_commands(app)
 
-app.register_blueprint(blog_bp)
-app.register_blueprint(admin_bp, url_prefix='/admin')
-app.register_blueprint(auth_bp, url_prefix='/auth')
+    return app
 
-@app.shell_context_processor
-def make_shell_context():
-    return dict(db=db)
+def register_logging(app):
+    pass
 
-@app.errorhandler(400)
-def bad_request(e):
-    return render_template('errors/400.html'), 400
+def register_extensions(app):
+    bootstrap.init_app(app)
+    db.init_app(app)
+    toolbar.init_app(app)
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('errors/404.html'), 404
+def register_blueprints(app):
+    app.register_blueprint(blog_bp)
+    app.register_blueprint(admin_bp, url_prefix='/admin')
+    app.register_blueprint(auth_bp, url_prefix='/auth')
 
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('errors/500.html'), 500
+def register_shell_context(app):
+    @app.shell_context_processor
+    def make_shell_context():
+        return dict(db=db)
 
+def register_template_context(app):
+    pass
 
+def register_errors(app):
+    @app.errorhandler(400)
+    def bad_request(e):
+        return render_template('errors/400.html'), 400
+
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return render_template('errors/404.html'), 404
+
+    @app.errorhandler(500)
+    def internal_server_error(e):
+        return render_template('errors/500.html'), 500
+
+def register_commands(app):
+    pass
